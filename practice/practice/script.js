f31b3c626936b32780f1b42f940636df0c6bb86b
huzaifaahmed190 <82325515+huzaifaahmed190@users.noreply.github.com>
2021-06-16 23:33:37 +0500

    projetct 1 signup form pratice
    index.html code

--- practice/practice/script.js
+++ practice/practice/script.js
@@ -1,16 +1,16 @@
-// Retrieving HTML elements from the DOM
+
 const form = document.getElementById('form');
-const username = document.getElementById('username');
+const firstname = document.getElementById('firstname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const password1 = document.getElementById('password1');
 
-// Function to update class and message for errors
+
 function showError(input, message) {
     // Get the parent element of the input field (.form-control)
     const formControl = input.parentElement;
     // Replace the class - add error
-    formControl.className = 'form-control error';
+    formControl.className = 'formcontrol error';
     // Get the small element for the error message
     const small = formControl.querySelector('small');
     // Replace the text for small element using the input message
@@ -22,7 +22,7 @@ function showSuccess(input) {
     // Get the parent element of the input field (.form-control)
     const formControl = input.parentElement;
     // Replace the class - add success
-    formControl.className = 'form-control success';
+    formControl.className = 'formcontrol success';
 };
 
 // Event Listeners
@@ -33,10 +33,10 @@ form.addEventListener('submit', function(e) {
 
     // Check to see if fields meet required field requirement
     // Check if username input is empty
-    if(username.value === '') {
-        showError(username, 'Username is required');
+    if(firstname.value === '') {
+        showError(firstname, 'Firstname is required');
     } else {
-        showSuccess(username);
+        showSuccess(firstname);
     };
 
     // Check if email input is empty
@@ -54,9 +54,9 @@ form.addEventListener('submit', function(e) {
     };
 
     // Check if password2 input is empty
-    if(password2.value === '') {
-        showError(password2, 'Confirm Password is required');
+    if(password1.value === '') {
+        showError(password1, 'Confirm Password is required');
     } else {
-        showSuccess(password2);
+        showSuccess(password1);
     };
 });
